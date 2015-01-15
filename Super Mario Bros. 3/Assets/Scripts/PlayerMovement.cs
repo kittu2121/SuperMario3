e9697b2e8caf9d1bb1598ff33e668559fdfb25b8
jonrim <jonrim@umich.edu>
2015-01-15 16:47:10 -0500

    .

---
+++ Super Mario Bros. 3/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float HorizSpeed = Input.GetAxis ("Horizontal");
+		if (HorizSpeed > 0) {
+			transform.localScale = new Vector3(-1, 1, 1);
+		}
+		else if (HorizSpeed < 0) {
+			transform.localScale = new Vector3(1, 1, 1);
+		}
+	}
+}
