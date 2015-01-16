9f038645126abf4be9ca9929ed861024300893ec
jonrim <jonrim@umich.edu>
2015-01-16 00:17:08 -0500

    imported mario and block sprites
    change scripts to work with 2d sprites

--- Super Mario Bros. 3/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros. 3/Assets/Scripts/PlayerMovement.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 
+	private Animator mario_anim;
 	// Use this for initialization
 	void Start () {
-	
+		mario_anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
 		float HorizSpeed = Input.GetAxis ("Horizontal");
+		mario_anim.SetFloat("Speed", Mathf.Abs(HorizSpeed));
 		if (HorizSpeed > 0) {
-			transform.localScale = new Vector3(-1, 1, 1);
+			transform.localScale = new Vector3(1, 1, 1);
 		}
 		else if (HorizSpeed < 0) {
-			transform.localScale = new Vector3(1, 1, 1);
+			transform.localScale = new Vector3(-1, 1, 1);
 		}
 	}
 }
