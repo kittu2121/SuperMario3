2f9016e1004fe058b48b76d4353a60a8374576c8
jonrim <jonrim@umich.edu>
2015-01-16 22:45:58 -0500

    collisions seem perfect, added coin, jump, walk

--- Super Mario Bros. 3/Assets/Scripts/PE_Obj2D.cs
+++ Super Mario Bros. 3/Assets/Scripts/PE_Obj2D.cs
@@ -61,104 +61,108 @@ public class PE_Obj2D : MonoBehaviour {
 				//Vector2 overlap = Vector2.zero;
 				thatP = that.transform.position;
 				delta = pos1 - thatP;
-				if (delta.x >= 0 && delta.y >= 0) { // Top, Right
-					// Get the edges that we're concerned with
-					eX0 = pos0.x - this.transform.lossyScale.x / 2;
-					eY0 = pos0.y - this.transform.lossyScale.y / 2;
-					eX1 = pos1.x - this.transform.lossyScale.x / 2;
-					eY1 = pos1.y - this.transform.lossyScale.y / 2;
-					eX2 = thatP.x + that.transform.lossyScale.x / 2 ;
-					eY2 = thatP.y + that.transform.lossyScale.y / 2 ;
+				if ((this.transform.position.x <= thatP.x + that.collider2D.bounds.size.x/2 ) &&
+				    (this.transform.position.x >= thatP.x - that.collider2D.bounds.size.x/2 )) { // if the center of this obj is between the x-bounds of that obj
 
-					if ((Mathf.Abs(eY1 - eY2) <= 0.1)) {// land on top
-						float dist = this.transform.lossyScale.y/2 + that.transform.lossyScale.y/2;
+					if (pos1.y >= thatP.y) { // land on top
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
 						vel.y = 0;
-						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y + dist);
+						acc.y = 0;
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y + dist+0.01f);
+						this.transform.position = pos;
+					}
+					else { // hit the bottom
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
+						vel.y = 0;
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y - dist-0.03f);
+						this.transform.position = pos;
+					}
+				}
+
+				else if (delta.x >= 0 && delta.y >= 0) { // Top, Right
+					// Get the edges that we're concerned with
+					eX0 = pos0.x - this.collider2D.bounds.size.x / 2; // prev Left side of object.
+					eY0 = pos0.y - this.collider2D.bounds.size.y / 2; // prev bottom side
+					eX1 = pos1.x - this.collider2D.bounds.size.x / 2; // current right side
+					eY1 = pos1.y - this.collider2D.bounds.size.y / 2; // current bottom side
+					eX2 = thatP.x + that.collider2D.bounds.size.x / 2 ; // other object's right side 
+					eY2 = thatP.y + that.collider2D.bounds.size.y / 2 ; // other object's  top side.
+					if ((Mathf.Abs(eY0 - eY2) <= 0.1)) {// land on top
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
+						vel.y = 0;
+						acc.y = 0;
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y + dist + 0.01f);
 						this.transform.position = pos;
 					}
 					else { // hit the right side
-						float dist = this.transform.lossyScale.x/2 + that.transform.lossyScale.x/2;
+						float dist = this.collider2D.bounds.size.x/2 + that.collider2D.bounds.size.x/2;
 						vel.x = 0;
 						acc.x = 0;
 						Vector2 pos = new Vector2(that.transform.position.x + dist, this.transform.position.y);
 						this.transform.position = pos;
 					}
-					// Distance traveled this step
-//					dX = eX1-eX0;
-//					dY = eY1-eY0;
-//					float uX = 1;
-//					float uY = 1;
-//					if (eX1 < eX2) { // Overlap in X direction
-//						uX = 1 - (eX2-eX1) / (eX0-eX1);
-//						
-//					}
-//					if (eY1 < eY2) { // Overlap in Y direction
-//						uY = 1 - (eY2-eY1) / (eY0-eY1);
-//						
-//					}
-					// Find Overlaps (positive is an overlap, negative 
-					// overlap.x = 
 
 				} else if (delta.x >= 0 && delta.y < 0) { // Bottom, Right
-					eX0 = pos0.x - this.transform.lossyScale.x / 2;
-					eY0 = pos0.y + this.transform.lossyScale.y / 2;
-					eX1 = pos1.x - this.transform.lossyScale.x / 2;
-					eY1 = pos1.y + this.transform.lossyScale.y / 2;
-					eX2 = thatP.x + that.transform.lossyScale.x / 2 ;
-					eY2 = thatP.y - that.transform.lossyScale.y / 2 ;
+					eX0 = pos0.x - this.collider2D.bounds.size.x / 2;
+					eY0 = pos0.y + this.collider2D.bounds.size.y / 2;
+					eX1 = pos1.x - this.collider2D.bounds.size.x / 2;
+					eY1 = pos1.y + this.collider2D.bounds.size.y / 2;
+					eX2 = thatP.x + that.collider2D.bounds.size.x / 2 ;
+					eY2 = thatP.y - that.collider2D.bounds.size.y / 2 ;
 
 					//print (eY1); print (eY2);
 					//if ((mThis > mThat) || ((eX0 == eX1) && (Mathf.Abs (mThat) <= 0.1) && (eX0 != eX2))) {// hit the bottom
-					if ((Mathf.Abs(eY1 - eY2) <= 0.1) && (Mathf.Abs(eX1 - eX2) >= 0.3)) {// hit the bottom
-						float dist = this.transform.lossyScale.y/2 + that.transform.lossyScale.y/2;
+					if ((Mathf.Abs(eY1 - eY2) <= 0.1) && (Mathf.Abs(eX1 - eX2) >= 0.4)) {// hit the bottom
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
 						vel.y = 0;
-						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y - dist);
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y - dist - 0.03f);
 						this.transform.position = pos;
 					}
 					else { // hit the right side
-						float dist = this.transform.lossyScale.x/2 + that.transform.lossyScale.x/2;
+						float dist = this.collider2D.bounds.size.x/2 + that.collider2D.bounds.size.x/2;
 						vel.x = 0;
 						acc.x = 0;
-						Vector2 pos = new Vector2(that.transform.position.x + dist, this.transform.position.y);
+						Vector2 pos = new Vector2(that.transform.position.x + dist + 0.06f, this.transform.position.y);
 						this.transform.position = pos;
 					}
 				} else if (delta.x < 0 && delta.y < 0) { // Bottom, Left
-					eX0 = pos0.x + this.transform.lossyScale.x / 2;
-					eY0 = pos0.y + this.transform.lossyScale.y / 2;
-					eX1 = pos1.x + this.transform.lossyScale.x / 2;
-					eY1 = pos1.y + this.transform.lossyScale.y / 2;
-					eX2 = thatP.x - that.transform.lossyScale.x / 2 ;
-					eY2 = thatP.y - that.transform.lossyScale.y / 2 ;
+					eX0 = pos0.x + this.collider2D.bounds.size.x / 2;
+					eY0 = pos0.y + this.collider2D.bounds.size.y / 2;
+					eX1 = pos1.x + this.collider2D.bounds.size.x / 2;
+					eY1 = pos1.y + this.collider2D.bounds.size.y / 2;
+					eX2 = thatP.x - that.collider2D.bounds.size.x / 2 ;
+					eY2 = thatP.y - that.collider2D.bounds.size.y / 2 ;
 
-					if ((Mathf.Abs(eY1 - eY2) <= 0.1) && (Mathf.Abs(eX1 - eX2) >= 0.3)) {// hit the bottom
-						float dist = this.transform.lossyScale.y/2 + that.transform.lossyScale.y/2;
+					if ((Mathf.Abs(eY1 - eY2) <= 0.1) && (Mathf.Abs(eX1 - eX2) >= 0.4)) {// hit the bottom
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
 						vel.y = 0;
-						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y - dist);
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y - dist - 0.03f);
 						this.transform.position = pos;
 					}
 					else { // hit the left side
-						float dist = this.transform.lossyScale.x/2 + that.transform.lossyScale.x/2;
+						float dist = this.collider2D.bounds.size.x/2 + that.collider2D.bounds.size.x/2;
 						vel.x = 0;
 						acc.x = 0;
-						Vector2 pos = new Vector2(that.transform.position.x - dist, this.transform.position.y);
+						Vector2 pos = new Vector2(that.transform.position.x - dist - 0.06f, this.transform.position.y);
 						this.transform.position = pos;
 					}
 				} else if (delta.x < 0 && delta.y >= 0) { // Top, Left
-					eX0 = pos0.x + this.transform.lossyScale.x / 2;
-					eY0 = pos0.y - this.transform.lossyScale.y / 2;
-					eX1 = pos1.x + this.transform.lossyScale.x / 2;
-					eY1 = pos1.y - this.transform.lossyScale.y / 2;
-					eX2 = thatP.x - that.transform.lossyScale.x / 2 ;
-					eY2 = thatP.y + that.transform.lossyScale.y / 2 ;
+					eX0 = pos0.x + this.collider2D.bounds.size.x / 2;
+					eY0 = pos0.y - this.collider2D.bounds.size.y / 2;
+					eX1 = pos1.x + this.collider2D.bounds.size.x / 2;
+					eY1 = pos1.y - this.collider2D.bounds.size.y / 2;
+					eX2 = thatP.x - that.collider2D.bounds.size.x / 2 ;
+					eY2 = thatP.y + that.collider2D.bounds.size.y / 2 ;
 
 					if (Mathf.Abs(eY1 - eY2) <= 0.1) {// land on top
-						float dist = this.transform.lossyScale.y/2 + that.transform.lossyScale.y/2;
+						float dist = this.collider2D.bounds.size.y/2 + that.collider2D.bounds.size.y/2;
 						vel.y = 0;
-						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y + dist);
+						acc.y = 0;
+						Vector2 pos = new Vector2(this.transform.position.x, that.transform.position.y + dist + 0.01f);
 						this.transform.position = pos;
 					}
 					else { // hit the left side
-						float dist = this.transform.lossyScale.x/2 + that.transform.lossyScale.x/2;
+						float dist = this.collider2D.bounds.size.x/2 + that.collider2D.bounds.size.x/2;
 						vel.x = 0;
 						acc.x = 0;
 						Vector2 pos = new Vector2(that.transform.position.x - dist, this.transform.position.y);
--- Super Mario Bros. 3/Assets/Scripts/PlayerMovement.cs
+++ Super Mario Bros. 3/Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,37 @@
 using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
-
+	public float SpeedMultiplier = 3.0f;
+	public LayerMask GroundLayers;
 	private Animator mario_anim;
+	private Transform is_on_ground;
 	// Use this for initialization
 	void Start () {
 		mario_anim = GetComponent<Animator>();
+		is_on_ground = transform.FindChild("IsOnGround");
+		//canJump = is_on_ground.GetComponent<Jump>().canJump;
 	}
-	
+
 	// Update is called once per frame
 	void FixedUpdate () {
+		Vector2 point1 = new Vector2(is_on_ground.transform.position.x - is_on_ground.collider2D.bounds.size.x/2, 
+		                             is_on_ground.transform.position.y - is_on_ground.collider2D.bounds.size.y/2);
+		Vector2 point2 = new Vector2(is_on_ground.transform.position.x + is_on_ground.collider2D.bounds.size.x/2, 
+		                             is_on_ground.transform.position.y + is_on_ground.collider2D.bounds.size.y/2);
+		bool canJump = Physics2D.OverlapArea(point1, point2, GroundLayers, 0, 0);
+		// next bool needed so that you can't jump off walls
+		bool canJump2 = Physics2D.OverlapPoint(is_on_ground.position, GroundLayers);
+		if (Input.GetButton ("Jump")) {
+			if ((canJump) && (canJump2)){
+				GetComponent<PE_Obj2D>().vel.y = 10.0f;
+				canJump = false;
+				canJump2 = false;
+			}
+		}
+		if (!canJump){
+			GetComponent<PE_Obj2D>().acc.y = -30.0f;
+		}
+		mario_anim.SetBool ("CanJump", canJump);
 		float HorizSpeed = Input.GetAxis ("Horizontal");
 		mario_anim.SetFloat("Speed", Mathf.Abs(HorizSpeed));
 		if (HorizSpeed > 0) {
@@ -19,5 +41,7 @@ public class PlayerMovement : MonoBehaviour {
 		else if (HorizSpeed < 0) {
 			transform.localScale = new Vector3(-1, 1, 1);
 		}
+		this.rigidbody2D.velocity = new Vector2(HorizSpeed*SpeedMultiplier, this.rigidbody2D.velocity.y);
+
 	}
 }
